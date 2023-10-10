038043a376c63e79f6324b18be63fea4ccf55c10
developersharif <developersharif@yahoo.com>
2023-10-10 09:00:50 +0600

    doc updated

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -38,12 +38,15 @@ class CommentController extends Controller
      */
     public function store(StoreCommentRequest $request)
     {
+        //Find the post associated with the $request->post_id
         $post = Post::find($request->post_id);
+        // Create a new comment
         Comment::create([
             'user_id' => Auth::user()->id,
             'post_id' => $request->post_id,
             'content' => $request->content,
         ]);
+        // Check if the user replying to the comment is not the same as the user who made the parent comment
         if($post->user_id != Auth::user()->id){
             $post->user->notify(new PostCommenteddNotification($post->id));
         }
@@ -58,15 +61,20 @@ class CommentController extends Controller
     */
     public function reply(UpdateCommentRequest $request)
     {
+        // Find the parent comment based on the provided parent_comment_id
         $comment = Comment::find($request->parent_comment_id);
+        // Find the user who created the parent comment
         $user = User::find($comment->user_id);
+        // Create a new comment with the provided data
         Comment::create([
             'user_id' => Auth::user()->id,
             'post_id' => $request->post_id,
             'parent_comment_id' => $request->parent_comment_id,
             'content' => $request->content,
         ]);
+        // Check if the user replying to the comment is not the same as the user who made the parent comment
         if($comment->user_id != Auth::user()->id){
+        // Notify the user who made the parent comment about the reply
         $user->notify(new PostRepliedNotification($comment->post_id));
         }
         return back();
@@ -103,12 +111,17 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
+        // Find the associated post of the comment
         $post =Post::find($comment->post_id);
+        // Authorize the deletion of the comment
         $this->authorize("delete",$comment);
+        // Delete the comment
         $comment->delete();
+        // Delete notifications related to the post
         $post->user->Notifications()->where('type', PostCommenteddNotification::class)
         ->orWhere('type',PostRepliedNotification::class)
                 ->where('data->postId', $post->id)->delete();
+        // Redirect back to the previous page
         return back();
     }
 }

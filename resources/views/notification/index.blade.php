8722049f9d764e6f033910763a2bef834d385edd
developersharif <developersharif@yahoo.com>
2023-10-10 09:39:46 +0600

    delete option added

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -31,13 +31,15 @@
                                 </a>
                                 <div tabindex="0" aria-label="close icon" role="button"
                                     class="focus:outline-none cursor-pointer">
-                                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M10.5 3.5L3.5 10.5" stroke="#4B5563" stroke-width="1.25"
-                                            stroke-linecap="round" stroke-linejoin="round" />
-                                        <path d="M3.5 3.5L10.5 10.5" stroke="#4B5563" stroke-width="1.25"
-                                            stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
+                                    <a href="{{ route('notification.delete', $notification->id) }}">
+                                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10.5 3.5L3.5 10.5" stroke="#4B5563" stroke-width="1.25"
+                                                stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M3.5 3.5L10.5 10.5" stroke="#4B5563" stroke-width="1.25"
+                                                stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                    </a>
                                 </div>
                             </div>
                             <p tabindex="0" class="focus:outline-none text-xs leading-3 pt-1 text-gray-500">

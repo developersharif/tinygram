bc689b865ead1ec1123d22e47ecaccff01b12294
developersharif <developersharif@yahoo.com>
2023-10-14 08:27:05 +0600

    post service added

--- app/Http/Controllers/Web/PostController.php
+++ app/Http/Controllers/Web/PostController.php
@@ -6,33 +6,27 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
 use App\Models\User;
+use App\Services\PostService;
 use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
 class PostController extends Controller
 {
+    protected $postService;
+    function __construct(PostService $postService){
+        $this->postService = $postService;
+    }
     public function indexApi(Request $request)
     {
-        $user = auth()->user();
-        $posts = $user->followingPosts();
-        return response()->json($posts);
-}
+        return response()->json($this->postService->newsFeed());
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $user = auth()->user();
-        // $posts = Post::with('user')
-        // ->with("likedBy")
-        // ->where('status', 1)
-        // ->whereHas('user', function ($query) {
-        //     $query->where('status', 1);
-        // })
-        // ->orderBy('id', 'desc')
-        // ->get();
-        $posts = $user->followingPosts();
+        $posts = $this->postService->newsFeed();
         return view('home', ['posts' => $posts]);
     }
 
@@ -50,16 +44,7 @@ class PostController extends Controller
     public function store(StorePostRequest $request)
     {
         try {
-        $postBody = $request->body;
-        $photo = $request->file('photo');
-        $photo->store("public/photos");
-        $photoName = $photo->hashName();
-
-        $post = new Post();
-        $post->user_id = Auth::user()->id;
-        $post->body = $postBody;
-        $post->image = $photoName;
-        $post->save();
+        $this->postService->createPost($request);
         return redirect()->route('home')->with('post', 'post created successfully');
         } catch (PostTooLargeException $e) {
             throw new PostTooLargeException("Error: {$e->getMessage()}", $e);
@@ -72,15 +57,8 @@ class PostController extends Controller
     public function show(string $id)
     {
         try {
-            $post = Post::find($id);
-        if (request('ref') == 'notification') {
-            $notification = auth()->user()->Notifications()
-            // ->where('type', PostLikedNotification::class)
-                ->where('data->postId', $id)->get();
-            $notification->markAsRead();
-        }
-        $comments = $post->comments()->whereNull('parent_comment_id')->with('childComments')->orderBy('id', 'desc')->get();
-        return view('post.view', ['post' => $post, "comments" => $comments]);
+           $data = $this->postService->singlePost($id);
+        return view('post.view', ["post" => $data["post"], "comments" => $data["comments"]]);
         } catch (\Throwable $th) {
             throw $th;
         }
@@ -91,7 +69,7 @@ class PostController extends Controller
      */
     public function edit(string $id)
     {
-        $post = Post::find($id);
+        $post = $this->postService->getPost($id);
         $this->authorize('update', $post);
         return view('post.edit', ['post' => $post]);
     }
@@ -102,22 +80,8 @@ class PostController extends Controller
     public function update(StorePostRequest $request, string $id)
     {
         try {
-            $post = Post::find($id);
-        if ($request->hasFile('photo')) {
-            $postBody = $request->body;
-            $photo = $request->file('photo');
-            $photo->store("public/photos");
-            $photoName = $photo->hashName();
-            $post->body = $postBody;
-            $post->image = $photoName;
-            $post->update();
+            $this->postService->updatePost($id, $request);
             return redirect()->route('home')->with('post', 'post updated successfully');
-        } else {
-            $postBody = $request->body;
-            $post->body = $postBody;
-            $post->update();
-            return redirect()->route('home')->with('post', 'post updated successfully');
-        }
         } catch (\Throwable $th) {
             throw $th;
         }
@@ -126,13 +90,13 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
         try {
-            $post = Post::find($id);
-        $this->authorize('delete', $post);
-        $post->delete();
-        return redirect()->back();
+            $post = $this->postService->getPost($id);
+            $this->authorize('delete', $post);
+            $this->postService->deletePost($post);
+        return redirect()->route("user.profile",$post->user->username);
         } catch (\Throwable $th) {
             throw $th;
         }

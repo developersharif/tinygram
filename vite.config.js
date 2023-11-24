05d32b615a5e4597f8044c7b7fcd84933edc68be
developersharif <developersharif@yahoo.com>
2023-11-24 23:56:15 +0600

    react config added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -7,8 +8,10 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/chat/main.jsx',
             ],
-            refresh: true,
+            // refresh: true,
         }),
+        react(),
     ],
 });

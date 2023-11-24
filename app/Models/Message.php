547a9ed14bbcc89596a609e46e9873ea8dd68a38
developersharif <developersharif@yahoo.com>
2023-11-25 00:01:55 +0600

    Message Model added

---
+++ app/Models/Message.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+    protected $fillable = ['sender_id','receiver_id','message','seen'];
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use App\Models\Message;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -156,6 +156,11 @@ class User extends Authenticatable
         ->orderBy('id', 'desc')->simplePaginate(3);
 
         return $posts;
+    }
+
+    public function sentMessages()
+    {
+        return $this->hasMany(Message::class, 'sender_id');
     }
 
 }

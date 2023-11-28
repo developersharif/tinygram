90c4980a5c9bf9946d92e628e8602c65317213d2
developersharif <developersharif@yahoo.com>
2023-11-28 19:14:50 +0600

    comment removed

--- resources/js/chat/App.jsx
+++ resources/js/chat/App.jsx
@@ -1,29 +1,8 @@
 import Home from "./components/chat/Home"
-// import "./assets/style.css"
 import { RouterProvider } from "react-router-dom"
 import Web from "./components/Web"
-// import { GlobalData } from "./provider/GlobalData"
-// import { useEffect, useState } from "react"
 import ChatProvicer from "./provider/ChatProvicer"
 function App() {
-  // const [data, setData] = useState();
-  // useEffect(() => {
-  //   const loadConversations = async () => {
-  //     const req = await fetch(`http://127.0.0.1:8000/api/chat/conversations`);
-  //     const conversations = await req.json();
-  //     setData((prevData) => ({ ...prevData, conversations: conversations }));
-  //   };
-
-  //   const loadUser = async () => {
-  //     let response = await fetch(`http://127.0.0.1:8000/api/user`);
-  //     let user = await response.json();
-  //     setData((prevData) => ({ ...prevData, user: user }));
-  //   };
-
-  //   loadConversations();
-  //   loadUser();
-  // }, []); 
-
   return (
     <ChatProvicer>
       <RouterProvider router={Web}>
@@ -32,6 +11,4 @@ function App() {
       </ChatProvicer>
   );
 }
-
-
 export default App

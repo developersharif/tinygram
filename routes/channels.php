b59a0a2a18cdf03cea6f669a4812c83969e7336e
developersharif <developersharif@yahoo.com>
2023-11-28 11:56:34 +0600

    chatRoom Auth logic updated

--- routes/channels.php
+++ routes/channels.php
@@ -18,6 +18,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('ChatRoom.{id}', function ($user, $id) {
-    return (int) $user->id !== (int) $id;
+    return (int) $user->id === (int) $id;
     // return true;
 });
